5e39c6732dc5a3529b53efb6a322a5f0a84a415a
grdaimap <36882078+grdaimap@users.noreply.github.com>
2019-02-12 20:35:25 +0800

    希望破灭*3

--- C#/ClassGrd/ClassGrd/Grd.cs
+++ C#/ClassGrd/ClassGrd/Grd.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
 namespace ClassGrd
 {
 	/// <summary>
@@ -9,39 +8,13 @@ namespace ClassGrd
 	/// </summary>
 	public partial class Grd
 	{
-
-		/// <summary>
-		/// 训练类
-		/// </summary>
-		public class Train
-		{
-
-			/// <summary>
-			/// 自动梯度机
-			/// </summary>
-			public class GradientDescentOptimizer
-			{
-				private double targetmap;
-				public void Minimize(double entropy)
-				{
-					targetmap = entropy;
-				}
-				public GradientDescentOptimizer(double rate = 0.01f)
-				{
-
-				}
-			}
-		}
-
 		/// <summary>
 		/// 启动训练
 		/// </summary>
 		/// <param name="train">训练器</param>
 		public void Run(Train train)
 		{
-
 		}
-
 		/// <summary>
 		/// 启动顺序运算
 		/// </summary>
@@ -50,7 +23,6 @@ namespace ClassGrd
 		{
 			map.Run();
 		}
-
 		/// <summary>
 		/// 矩阵乘法构造新图
 		/// </summary>
@@ -60,14 +32,13 @@ namespace ClassGrd
 		public static Map Matmul(Map a, Map b)
 		{
 			Map c = a * b;
-			if (a.premaps != null) c.premaps.AddRange(a.premaps);
-			else c.premaps.Add(a);
-			if (b.premaps != null) c.premaps.AddRange(b.premaps);
-			else c.premaps.Add(b);
+			c.premaps.Add(a);
+			c.premaps.Add(b);
+
+			c.sys = "*";
 
 			return c;
 		}
-
 		/// <summary>
 		/// 矩阵加法构造新图
 		/// </summary>
@@ -77,11 +48,10 @@ namespace ClassGrd
 		public static Map Plus(Map a, Map b)
 		{
 			Map c = a + b;
-			if (a.premaps != null) c.premaps.AddRange(a.premaps);
-			else c.premaps.Add(a);
-			if (b.premaps != null) c.premaps.AddRange(b.premaps);
-			else c.premaps.Add(b);
+			c.premaps.Add(a);
+			c.premaps.Add(b);
 
+			c.sys = "+";
 			return c;
 		}
 	}
--- C#/ClassGrd/ClassGrd/Map.cs
+++ C#/ClassGrd/ClassGrd/Map.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
 namespace ClassGrd
 {
 	/// <summary>
@@ -13,87 +12,57 @@ namespace ClassGrd
 		/// 名称
 		/// </summary>
 		public string name;
-
 		/// <summary>
 		/// 形状，先列数后行数
 		/// </summary>
 		public int[] shape;
-
 		/// <summary>
 		/// 数据，二维数组
 		/// </summary>
 		public double[,] data;
-
 		/// <summary>
 		/// 需要之前就算好的图的列表
 		/// </summary>
 		public List<Map> premaps;
-
 		/// <summary>
-		/// 记号列表，把构造和执行分开
+		/// 记号，把构造和执行分开
 		/// </summary>
-		public List<int> sys;
-
+		public string sys = "";
 		/// <summary>
 		/// 是否接受网络训练
 		/// </summary>
 		public bool trainable = true;
-
 		/// <summary>
 		/// 运行
 		/// </summary>
 		public void Run()
 		{
-			if (trainable == false) return;
-			int c = 0;
-			Map new_map = premaps[0];
-			foreach (int item in sys)
-				switch (item)
-				{
-					case 0: break;
-					case 1: new_map = new_map + premaps[c + 1]; break;
-					case 2: new_map = new_map * premaps[c + 1]; break;
-					case 3: break;
-					case 4: break;
-					case 5: break;
-					case 6: break;
-					case 7: break;
-					case 8: break;
-					case 9: break;
-					case 10: break;
-					case 11: break;
-					case 12: break;
-					case 13: break;
-					case 14: break;
-					case 15: break;
-					case 16: break;
-					case 17: break;
-					case 18: break;
-					case 19: break;
-					case 20: break;
-					case 21: break;
-					case 22: break;
-					case 23: break;
-					default: c++; break;
-				}
+			if (sys == "") return;
+			premaps[0].Run();
+			premaps[1].Run();
+			Map new_map = new Map(shape[0], shape[1]);
+			switch (sys)
+			{
+				case "": break;
+				case "+": new_map = premaps[0] + premaps[1]; break;
+				case "*": new_map = premaps[0] * premaps[1]; break;
+				default: break;
+			}
 			data = new_map.data;
 		}
-
 		/// <summary>
 		/// 改变形状
 		/// </summary>
 		/// <param name="columns">新矩阵行数</param>
 		public void Reshape(int columns = 1)
 		{
-
 		}
-
 		/// <summary>
 		/// 摊平data数组
 		/// </summary>
 		public Map Plain()
 		{
-			Map c=new Map(shape[0] * shape[1], 1);
+			Map c = new Map(shape[0] * shape[1], 1);
 			double[,] new_data = new double[shape[0] * shape[1], 1];
 			for (int cl = 0; cl < shape[0]; cl++)
 				for (int r = 0; r < shape[1]; r++)
@@ -101,19 +70,19 @@ namespace ClassGrd
 			c.data = new_data;
 			return c;
 		}
-
 		/// <summary>
 		/// 绑定数据来源
 		/// </summary>
 		/// <param name="arr">一个必须符合data大小的二维数组</param>
 		public void Feed(double[,] arr)
 		{
-			data = arr;
-			//int arcl = arr.GetLength(0);
-			//int arro = arr.GetLength(1);
-			trainable = false;
+			if (shape[0] == arr.GetLength(0) && shape[1] == arr.GetLength(1))
+			{
+				data = arr;
+				trainable = false;
+			}
+			else throw new Exception("检查数组形状");
 		}
-
 		/// <summary>
 		/// 图的构造函数
 		/// </summary>
@@ -126,7 +95,6 @@ namespace ClassGrd
 			if (columns > 0 && rows > 0) shape = new int[] { columns, rows };
 			data = new double[columns, rows];
 		}
-
 		/// <summary>
 		/// 重载*运算，矩阵乘法
 		/// </summary>
@@ -147,11 +115,9 @@ namespace ClassGrd
 					for (int r = 0; r < c.shape[1]; r++)
 						for (int co = 0; co < a.shape[1]; co++)
 							c.data[cl, r] += a.data[cl, co] * b.data[co, r];
-
 				return c;
 			}
 		}
-
 		/// <summary>
 		/// 重载+运算，矩阵乘法
 		/// </summary>
@@ -167,7 +133,6 @@ namespace ClassGrd
 				for (int cl = 0; cl < c.shape[0]; cl++)
 					for (int r = 0; r < c.shape[1]; r++)
 						c.data[cl, r] = a.data[cl, r] + b.data[cl, r];
-
 				return c;
 			}
 			else
